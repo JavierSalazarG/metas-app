3e930c714fc8ff9bbdd226f45f7cb04554f1f4d1
Javier Salazar <javis9555@gmail.com>
2022-11-14 19:54:02 +0100

    instalacion tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Encabezamiento from './componentes/compartidos/Encabezamiento';
+import Principal from './componentes/compartidos/Principal';
+import Pie from './componentes/compartidos/Pie';
 
 function App() {
   return (
     <div className="App">
-
+      <Encabezamiento ></Encabezamiento>
+      <Principal ></Principal>
+      <Pie ></Pie>
     </div>
   );
 }
---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,11 @@
+
+function Pie() {
+    return (
+        <footer className="text-red-500">
+            <small>Desarrollado y diseñado por Javier Salazar Gutiérrez ©️2030. Todos los derechos reservados</small>
+
+        </footer>
+    );
+}
+
+export default Pie;
